2005ed29252e790211a128c88ecebf960738b234
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-02 21:11:41 +0100

    I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  add_flash_types :success, :danger
-  helper_method :current_user, :user_signed_in?
+    protect_from_forgery with: :exception
 
-  private
-  def only_signed_in
-      if !user_signed_in?
-          redirect_to new_user_path, danger: "Vous n'avez pas le droit d'accéder à cette page"
-      end
-  end
+    private
 
-  def only_signed_out
-      redirect_to profil_path if user_signed_in?
-  end
-
-  def user_signed_in?
-      !current_user.nil?
-  end
-
-  # Retourne l'utilisateur actuellement connecté
-  def current_user
-      return nil if !session[:auth] || !session[:auth]['id']
-      return @_user if @_user
-      @_user = User.find_by_id(session[:auth]['id'])
-  end
+    def set_locale
+        I18n.locale = "fr"
+    end
 end
