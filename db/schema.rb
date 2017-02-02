a017fc02c41f90297ea9c009302bdf159a2a506c
tboucheau <boucheau.tony@neuf.fr>
2017-02-02 18:09:10 +0000

    add published

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202020635) do
+ActiveRecord::Schema.define(version: 20170202180822) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170202020635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.boolean  "published"
     t.index ["user_id"], name: "index_advertisements_on_user_id"
   end
 
