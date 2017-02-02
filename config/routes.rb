f2a517f1a8698ac465637d3f3d88f74e56a28042
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-02 02:02:48 +0100

    advertisement

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
   get 'advertisements/index'
-
   get 'advertisements/show'
-
   get 'advertisements/new'
-
   get 'advertisements/create'
-
   get 'advertisements/update'
-
   get 'advertisements/destroy'
-
   get 'advertisements/edit'
-
   get 'users/new'
-
   get 'users/login'
-
+  root 'advertisements#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
