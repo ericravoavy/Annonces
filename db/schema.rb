a4ff2ab62cbfb46dafb7cc8a715be55b26c5d675
tboucheau <boucheau.tony@neuf.fr>
2017-02-02 02:13:07 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202013145) do
+ActiveRecord::Schema.define(version: 20170202020635) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170202013145) do
     t.string   "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_advertisements_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
