d021d7ef226ae6ed0b51355d23d81f1afa418351
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-03 10:56:33 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'users/login' => 'users#login', as: 'login'
   post 'users/login' => 'users#check'
+  patch 'advertisements/:id' => 'advertisements#publish'
   resources :users
   resources :advertisements
-  patch 'advertisements/:id' => 'advertisements#publish'
   post 'advertisements/new' => 'advertisements#new'
   root 'advertisements#index'
   get '*path' => redirect('/')
