0dfc55f40af2a64ace4649c9d85e4767515bcaae
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-03 15:52:57 +0100

    mini pass = 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true,
         format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumériques ou des _'},
         uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
